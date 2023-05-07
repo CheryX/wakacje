0e1ce747f82ed42dcbd4beb57f02faaa13ed438e
CheryX <maciejmusielik@gmail.com>
2023-05-07 20:00:13 +0200

    Move some holidays from settings

--- src/settings.js
+++ src/settings.js
@@ -56,27 +56,12 @@ export default {
             occurences: [ 2, 4, 4 ]
         },
     ],
-    freeDays: [
-        "2023-01-01", // Nowy Rok
-        "2023-01-06", // Trzech Króli
-        "2023-04-10", // Poniedziałek Wielkanocny
-        "2023-05-01", // Święto Pracy
-        "2023-05-03", // Święto Konstytucji 3 Maja
-        "2023-05-28", // Zielone Świątki
-        "2023-06-08", // Boże Ciało
-        "2023-08-15", // Dzień Wojska Polskiego
-        "2023-11-01", // Wszystkich Świętych
-        "2023-11-11", // Dzień Niepodległości
-        "2023-12-25", // Boże Narodzenie (1 dzień)
-        "2023-12-26", // Boże Narodzenie (2 dzień)
-
-        // Wolne dni w szkole
-        "2023-05-02", // Święto Flagi
+    freeDays: [ // Wolne dni w szkole
+        "2023-05-02",
         "2023-05-04",
         "2023-05-05",
         "2023-05-08",
         "2023-05-09",
-
         "2023-05-24",
         "2023-05-25",
         "2023-05-26",
